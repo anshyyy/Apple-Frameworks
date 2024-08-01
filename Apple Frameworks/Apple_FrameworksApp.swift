8752c43a6354606775758da29a771d2c44cf7b8b
anshyyy <anshuman9998@gmail.com>
2024-08-01 12:21:10 +0530

    inital commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Anshuman Sharma on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
